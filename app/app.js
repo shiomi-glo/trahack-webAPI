eddb0a48df5d4569e418a69d69a20facd0a89d06
Tomoaki Shiomi <shiomi@glomouth.co.jp>
2021-01-02 17:25:49 +0900

    get methods

--- app/app.js
+++ app/app.js
@@ -1,9 +1,38 @@
 const express = require('express')
 const app = express()
+const sqlite3 = require('sqlite3')
+const dbPath = "app/db/database.sqlite3"
 
-app.get('/api/v1/users', (req, res) => {
-  res.json('{"message": "this will be user list"}')
+// Get a user
+app.get('/api/v1/users/:id', (req, res) => {
+  // Connect database
+  const db = new sqlite3.Database(dbPath)
+  const id = req.params.id
+
+  db.get(`SELECT * FROM users WHERE id = ${id}`, (err, row) => {
+    if (!row) {
+      res.status(404).send({error: "Not Found!"})
+    } else {
+      res.status(200).json(row)
+    }
+  })
+
+  db.close()
 })
+
+// Search users matching keyword
+app.get('/api/v1/search', (req, res) => {
+  // Connect database
+  const db = new sqlite3.Database(dbPath)
+  const keyword = req.query.q
+
+  db.all(`SELECT * FROM users WHERE name LIKE "%${keyword}%"`, (err, rows) => {
+    res.json(rows)
+  })
+
+  db.close()
+})
+
 console.log (process.env)
 const port = 3000;
 app.listen(port)
